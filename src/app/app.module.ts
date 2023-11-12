5643e01d8a4deceb652c5545fbf6e65a0a128250
darku <darkusagi99@gmail.com>
2023-11-12 17:22:01 +0100

    Init application look

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatIconModule, MatButtonToggleModule, MatButtonModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
