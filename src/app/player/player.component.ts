45f07edee236e32b49ad3770ea7f19fb514e5646
darku <darkusagi99@gmail.com>
2023-11-19 17:51:05 +0100

    Display list of players

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,19 +1,23 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 
+import { Player } from '../player';
+import {PLAYERS} from '../mock-player';
+
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css'],
-  imports:[CommonModule, MatCardModule, MatIconModule, MatButtonModule],
+  imports:[CommonModule, MatCardModule, MatIconModule, MatButtonModule, NgFor],
   standalone : true
 })
 export class PlayerComponent {
 	@Input() configurationMode = false;
-	name = '';
-	score = 1;
+	
+	players = PLAYERS;
+	
 }
