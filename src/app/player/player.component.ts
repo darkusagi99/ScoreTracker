fba9ae6a7b72d632dc63fed06b01b510033f29ab
darku <darkusagi99@gmail.com>
2023-11-19 18:06:14 +0100

    Delete one element

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -18,15 +18,41 @@ import {PLAYERS} from '../mock-player';
 export class PlayerComponent {
 	@Input() configurationMode = false;
 	
+	// Player list
 	players = PLAYERS;
 	
-	
+	// Decrease player score
 	decreaseScore(player : Player) : void {
 		player.score--;
 	}
 	
+	// Increate player score
 	increaseScore(player : Player) : void {
 		player.score++;
+	}
+	
+	// Remove a player from the list
+	removePlayer(player : Player) : void {
+		// Find index of player
+		const indexOfPlayer = this.players.findIndex((object) => {
+		  return object.id === player.id;
+		});
+	
+		// Deletion itself
+		if (indexOfPlayer !== -1) {
+			this.players.splice(indexOfPlayer, 1);
+		}
+	
+	}
+	
+	// Delete all players from the list
+	removeAllPlayers(player : Player) : void {
+		this.players.splice(0);
+	}
+	
+	// Add a player in the list
+	addPlayer(player : Player) : void {
+	
 	}
 	
 }
